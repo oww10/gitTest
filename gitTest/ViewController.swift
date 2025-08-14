94550bc68b206b852e8c720a999f528b5c32fecb
oww10 <dntjr2209@gmail.com>
2025-08-14 17:34:41 +0900

    Initial Commit

---
+++ gitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitTest
+//
+//  Created by s s on 8/14/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ gitTestTests/gitTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  gitTestTests.swift
+//  gitTestTests
+//
+//  Created by s s on 8/14/25.
+//
+
+import Testing
+@testable import gitTest
+
+struct gitTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
